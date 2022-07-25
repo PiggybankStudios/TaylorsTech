12d22c72bb1439889ad64764200aa33be2346286
Taylor Robbins (Piggybank Studios) <PrincessDungeonSmasher@gmail.com>
2022-07-24 20:55:16 -0700

    Added TaylorVersion.cs to 2019 project. Updated version number to 1.2

---
+++ 2019/TaylorVersion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaylorsTech
+{
+	class TaylorVersion
+	{
+		public static int Major = 1;
+		public static int Minor = 2;
+		public static int Build = 0;
+		public static int VsTarget = 2019;
+	}
+}
